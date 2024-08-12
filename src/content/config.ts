ac70542e0adabc477ccdaab52de15bb4aa1b5430
Emmanuel Mendez <emmanuelmendez.dev@outlook.com>
2024-08-12 15:52:09 -0400

    fix: create config collection and recreate each page collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,162 +1,76 @@
 import { PositionTypeOfCommerce } from "@/components/molecules/Position.astro";
+import { LAYOUT_DEFAULT_DATA } from "@/layouts/Layout.astro";
+import { DEFAULT_PAGE_NOT_FOUND_DATA } from "@/pages/404.astro";
+import { HOME_DEFAULT_DATA } from "@/pages/index.astro";
 import { z, defineCollection } from "astro:content";
-
-const DEFAULT_DATA = {
-  home: {
-    basis: {
-      fullName: "Full Name",
-      headline: "Headline",
-      image: "./images/me.webp",
-      email: "user@email.com",
-      phone: "000-000-0000",
-      url: "https://example.com",
-      profiles: [{
-        network: "LinkedIn",
-        username: "username-linkedin",
-        url: "https://linkedin.com/in/username-linkedin",
-      }],
-    },
-    about: {
-      title: 'About Me',
-      summary: '(number)+ years of experience creating (products) and specializing in (specializations).',
-    },
-    workExperience: {
-      title: 'Work Experience',
-      list: [
-        {
-          role: 'Role',
-          company: 'Company',
-          location: 'Miami, USA',
-          sector: 'Digital Production',
-          typeOfCommerce: PositionTypeOfCommerce.B2B,
-          startDate: new Date('2018-09-01'),
-          endDate: new Date('2022-05-31'),
-          responsibilities: ['Create', 'Maintain', 'Upgrade'],
-          keyAccomplishments: [
-            'Features implemented',
-            'Improvements made',
-            'Results achieved'
-          ],
-          url: 'https://example.com/my-project',
-          image: 'https://example.com/my-project.jpg',
-        }
-      ],
-    },
-    projects: {
-      title: 'Projects',
-      list: [
-        {
-          name: 'Project',
-          isActive: true,
-          description: 'Description of the project.',
-          highlights: [
-            'Features implemented',
-            'Improvements made',
-            'Results achieved',
-            'Technologies used',
-          ],
-          url: 'https://example.com/my-project',
-          image: 'https://example.com/my-project.jpg',
-        }
-      ]
-    },
-    education: {
-      title: 'Education',
-      list: [
-        {
-          institution: 'University',
-          degree: 'Degree',
-          startDate: new Date('2018-09-01'),
-          endDate: new Date('2022-05-31'),
-        }
-      ]
-    },
-    skills: {
-      title: 'Skills',
-      list: [
-        'Effective communication',
-        'Team collaboration',
-        'Problem-solving',
-        'Adaptability',
-        'Time management'
-      ]
-    }
-  },
-  pageNotFound: {
-    title: '404',
-    description: 'Page not found',
-    information: 'The page you are looking for does not exist.',
-    goBack: 'Go back to portfolio'
-  }
-};
 
 const homeCollection = defineCollection({
   type: "data",
   schema: z.object({
     basis: z.object({
-      fullName: z.string().optional().default(DEFAULT_DATA.home.basis.fullName),
-      headline: z.string().optional().default(DEFAULT_DATA.home.basis.headline),
-      image: z.string().optional().default(DEFAULT_DATA.home.basis.image),
-      email: z.string().optional().default(DEFAULT_DATA.home.basis.email),
-      phone: z.string().optional().default(DEFAULT_DATA.home.basis.phone),
-      url: z.string().optional().default(DEFAULT_DATA.home.basis.url),
+      fullName: z.string().optional().default(HOME_DEFAULT_DATA.basis.fullName),
+      headline: z.string().optional().default(HOME_DEFAULT_DATA.basis.headline),
+      image: z.string().optional().default(HOME_DEFAULT_DATA.basis.image),
+      email: z.string().optional().default(HOME_DEFAULT_DATA.basis.email),
+      phone: z.string().optional().default(HOME_DEFAULT_DATA.basis.phone),
+      url: z.string().optional().default(HOME_DEFAULT_DATA.basis.url),
       profiles: z.array(
         z.object({
-          network: z.string().optional().default(DEFAULT_DATA.home.basis.profiles[0]!.network),
-          username: z.string().optional().default(DEFAULT_DATA.home.basis.profiles[0]!.username),
-          url: z.string().optional().default(DEFAULT_DATA.home.basis.profiles[0]!.url),
+          network: z.string().optional().default(HOME_DEFAULT_DATA.basis.profiles[0]!.network),
+          username: z.string().optional().default(HOME_DEFAULT_DATA.basis.profiles[0]!.username),
+          url: z.string().optional().default(HOME_DEFAULT_DATA.basis.profiles[0]!.url),
         })
-      ).optional().default(DEFAULT_DATA.home.basis.profiles.map(({ ...profile }) => profile)),
+      ).optional().default(HOME_DEFAULT_DATA.basis.profiles.map(({ ...profile }) => profile)),
     }).optional(),
     about: z.object({
-      title: z.string().optional().default(DEFAULT_DATA.home.about.title),
-      summary: z.string().optional().default(DEFAULT_DATA.home.about.summary),
+      title: z.string().optional().default(HOME_DEFAULT_DATA.about.title),
+      summary: z.string().optional().default(HOME_DEFAULT_DATA.about.summary),
     }).optional(),
     workExperience: z.object({
-      title: z.string().optional().default(DEFAULT_DATA.home.workExperience.title),
+      title: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.title),
       list: z.array(
         z.object({
-          role: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.role),
-          company: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.company),
-          location: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.location),
-          sector: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.sector),
-          typeOfCommerce: z.nativeEnum(PositionTypeOfCommerce).optional().default(DEFAULT_DATA.home.workExperience.list[0]!.typeOfCommerce),
-          startDate: z.coerce.date().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.startDate),
-          endDate: z.coerce.date().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.endDate),
-          responsibilities: z.array(z.string()).optional().default(DEFAULT_DATA.home.workExperience.list[0]!.responsibilities),
-          keyAccomplishments: z.array(z.string()).optional().default(DEFAULT_DATA.home.workExperience.list[0]!.keyAccomplishments),
-          url: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.url),
-          image: z.string().optional().default(DEFAULT_DATA.home.workExperience.list[0]!.image),
+          role: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.role),
+          company: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.company),
+          location: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.location),
+          sector: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.sector),
+          typeOfCommerce: z.nativeEnum(PositionTypeOfCommerce).optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.typeOfCommerce),
+          startDate: z.coerce.date().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.startDate),
+          endDate: z.coerce.date().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.endDate),
+          responsibilities: z.array(z.string()).optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.responsibilities),
+          keyAccomplishments: z.array(z.string()).optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.keyAccomplishments),
+          url: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.url),
+          image: z.string().optional().default(HOME_DEFAULT_DATA.workExperience.list[0]!.image),
         })
-      ).optional().default(DEFAULT_DATA.home.workExperience.list.map(({ ...position }) => position)),
+      ).optional().default(HOME_DEFAULT_DATA.workExperience.list.map(({ ...position }) => position)),
     }).optional(),
     projects: z.object({
-      title: z.string().optional().default(DEFAULT_DATA.home.projects.title),
+      title: z.string().optional().default(HOME_DEFAULT_DATA.projects.title),
       list: z.array(
         z.object({
-          name: z.string().optional().default(DEFAULT_DATA.home.projects.list[0]!.name),
-          isActive: z.boolean().optional().default(DEFAULT_DATA.home.projects.list[0]!.isActive),
-          description: z.string().optional().default(DEFAULT_DATA.home.projects.list[0]!.description),
-          highlights: z.array(z.string()).optional().default(DEFAULT_DATA.home.projects.list[0]!.highlights),
-          url: z.string().optional().default(DEFAULT_DATA.home.projects.list[0]!.url),
-          image: z.string().optional().default(DEFAULT_DATA.home.projects.list[0]!.image),
+          name: z.string().optional().default(HOME_DEFAULT_DATA.projects.list[0]!.name),
+          isActive: z.boolean().optional().default(HOME_DEFAULT_DATA.projects.list[0]!.isActive),
+          description: z.string().optional().default(HOME_DEFAULT_DATA.projects.list[0]!.description),
+          highlights: z.array(z.string()).optional().default(HOME_DEFAULT_DATA.projects.list[0]!.highlights),
+          url: z.string().optional().default(HOME_DEFAULT_DATA.projects.list[0]!.url),
+          image: z.string().optional().default(HOME_DEFAULT_DATA.projects.list[0]!.image),
         })
-      ).optional().default(DEFAULT_DATA.home.projects.list.map(({ ...project }) => project)),
+      ).optional().default(HOME_DEFAULT_DATA.projects.list.map(({ ...project }) => project)),
     }).optional(),
     education: z.object({
-      title: z.string().optional().default(DEFAULT_DATA.home.education.title),
+      title: z.string().optional().default(HOME_DEFAULT_DATA.education.title),
       list: z.array(
         z.object({
-          institution: z.string().optional().default(DEFAULT_DATA.home.education.list[0]!.institution),
-          degree: z.string().optional().default(DEFAULT_DATA.home.education.list[0]!.degree),
-          startDate: z.coerce.date().optional().default(DEFAULT_DATA.home.education.list[0]!.startDate),
-          endDate: z.coerce.date().optional().default(DEFAULT_DATA.home.education.list[0]!.endDate),
+          institution: z.string().optional().default(HOME_DEFAULT_DATA.education.list[0]!.institution),
+          degree: z.string().optional().default(HOME_DEFAULT_DATA.education.list[0]!.degree),
+          startDate: z.coerce.date().optional().default(HOME_DEFAULT_DATA.education.list[0]!.startDate),
+          endDate: z.coerce.date().optional().default(HOME_DEFAULT_DATA.education.list[0]!.endDate),
         })
-      ).optional().default(DEFAULT_DATA.home.education.list.map(({ ...institution }) => institution)),
+      ).optional().default(HOME_DEFAULT_DATA.education.list.map(({ ...institution }) => institution)),
     }).optional(),
     skills: z.object({
-      title: z.string().optional().default(DEFAULT_DATA.home.skills.title),
-      list: z.array(z.string().optional().default(DEFAULT_DATA.home.skills.list[0]!)).optional().default(DEFAULT_DATA.home.skills.list.map((skill) => skill)),
+      title: z.string().optional().default(HOME_DEFAULT_DATA.skills.title),
+      list: z.array(z.string().optional().default(HOME_DEFAULT_DATA.skills.list[0]!)).optional().default(HOME_DEFAULT_DATA.skills.list.map((skill) => skill)),
     }).optional(),
   }),
 });
@@ -164,14 +78,29 @@ const homeCollection = defineCollection({
 const pageNotFoundCollection = defineCollection({
   type: "data",
   schema: z.object({
-    title: z.string().optional().default(DEFAULT_DATA.pageNotFound.title),
-    description: z.string().optional().default(DEFAULT_DATA.pageNotFound.description),
-    information: z.string().optional().default(DEFAULT_DATA.pageNotFound.information),
-    goBack: z.string().optional().default(DEFAULT_DATA.pageNotFound.goBack),
+    title: z.string().optional().default(DEFAULT_PAGE_NOT_FOUND_DATA.title),
+    description: z.string().optional().default(DEFAULT_PAGE_NOT_FOUND_DATA.description),
+    information: z.string().optional().default(DEFAULT_PAGE_NOT_FOUND_DATA.information),
+    goBack: z.string().optional().default(DEFAULT_PAGE_NOT_FOUND_DATA.goBack),
+  }),
+});
+
+const configCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    colors: z.object({
+      'primary': z.string().optional().default(LAYOUT_DEFAULT_DATA.primary),
+      'analogous--primary': z.string().optional().default(LAYOUT_DEFAULT_DATA.analogousPrimary),
+      'analogous--primary-50': z.string().optional().default(LAYOUT_DEFAULT_DATA.analogousPrimary50),
+      'analogous--secondary': z.string().optional().default(LAYOUT_DEFAULT_DATA.analogousSecondary),
+      'analogous--secondary-50': z.string().optional().default(LAYOUT_DEFAULT_DATA.analogousSecondary50),
+      'monochromatic--dark-deep': z.string().optional().default(LAYOUT_DEFAULT_DATA.monochromaticDarkDeep),
+    }).optional()
   }),
 });
 
 export const collections = {
-  home: homeCollection,
-  pageNotFound: pageNotFoundCollection,
+  homePage: homeCollection,
+  notFoundPage: pageNotFoundCollection,
+  config: configCollection
 };
